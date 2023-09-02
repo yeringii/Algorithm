66515ad40bffa6c5d9f68f00f9ed71739e21fcc8
yeringii <55120146+yeringii@users.noreply.github.com>
2023-09-02 21:11:34 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
@@ -8,7 +8,7 @@ count = 0
 for _ in range(n):
     #소 번호, 위치
     cow, location = map(int, sys.stdin.readline().split())
-    if cow in record: #아직 등록되지 않았다면 등록
+    if cow in record: #등록되었다면 위치 변경 확인
         if record[cow] != location:
             count += 1
     record[cow] = location
