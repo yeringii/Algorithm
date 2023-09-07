d725a21a46bd85c0811b0433756165e83ff54b49
yeringii <55120146+yeringii@users.noreply.github.com>
2023-09-08 00:09:30 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 104 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,12 @@
+'''
+알파벳 개수
+'''
+
+import sys
+
+S = sys.stdin.readline()
+
+alphabet_lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for i in alphabet_lst:
+    print(S.count(i),end=' ')
