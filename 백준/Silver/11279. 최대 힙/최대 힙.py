c58afbe52d24c8183223cca81b8e174e46447e89
yeringii <55120146+yeringii@users.noreply.github.com>
2023-09-09 16:40:29 +0900

    [Silver II] Title: 최대 힙, Time: 164 ms, Memory: 46396 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,13 @@
+import sys, heapq
+
+n = int(sys.stdin.readline())
+nums = []
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    if num != 0:
+        heapq.heappush(nums,(-num,num))
+    else:
+        if len(nums) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(nums)[1])
