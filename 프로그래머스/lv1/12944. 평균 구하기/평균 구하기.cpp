8bdcb9e482c2574d8af8c39aabb4875085ef586c
yeringii <55120146+yeringii@users.noreply.github.com>
2023-09-30 01:08:04 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    for(double num : arr){
+        answer += num;
+    }
+    answer /= arr.size();
+    return answer;
+}
